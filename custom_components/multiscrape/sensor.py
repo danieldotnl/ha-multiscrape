d563b57d5571263c3955c2e8caf9c7edfc959ffb
Daniel <danieldotnl@outlook.com>
2023-05-05 19:58:11 +0000

    Set unavailable property instead of value

--- custom_components/multiscrape/sensor.py
+++ custom_components/multiscrape/sensor.py
@@ -14,7 +14,6 @@ from homeassistant.const import CONF_RESOURCE_TEMPLATE
 from homeassistant.const import CONF_UNIQUE_ID
 from homeassistant.const import CONF_UNIT_OF_MEASUREMENT
 from homeassistant.const import Platform
-from homeassistant.const import STATE_UNAVAILABLE
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.entity import async_generate_entity_id
@@ -144,6 +143,7 @@ class MultiscrapeSensor(MultiscrapeEntity, SensorEntity):
         _LOGGER.debug(
             "%s # %s # Start scraping to update sensor", self.scraper.name, self._name
         )
+        self._attr_available = True
 
         try:
             if self.coordinator.update_error is True:
@@ -181,7 +181,7 @@ class MultiscrapeSensor(MultiscrapeEntity, SensorEntity):
                 )
 
             if self._sensor_selector.on_error.value == CONF_ON_ERROR_VALUE_NONE:
-                self._attr_native_value = STATE_UNAVAILABLE
+                self._attr_available = False
                 _LOGGER.debug(
                     "%s # %s # On-error, set value to None",
                     self.scraper.name,
