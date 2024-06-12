bab22aee8341fb0ad8bec82dd021ef7e353e8888
Milan Jeremic <milanjeremic@LIVE.COM>
2024-06-12 22:05:12 +0000

    Returning None by default if value_template is None

--- custom_components/multiscrape/util.py
+++ custom_components/multiscrape/util.py
@@ -10,7 +10,7 @@ _LOGGER: logging.Logger = logging.getLogger(__name__)
 def create_renderer(hass, value_template):
     """Create a template renderer based on value_template."""
     if value_template is None:
-        return lambda value=None, parse_result=None: value
+        return lambda variables={}, parse_result=None: None
 
     if not isinstance(value_template, Template):
         value_template = Template(value_template, hass)
@@ -32,7 +32,7 @@ def create_renderer(hass, value_template):
 def create_dict_renderer(hass, templates_dict):
     """Create template renderers for a dictionary with value_templates."""
     if templates_dict is None:
-        return lambda value=None, parse_result=None: {}
+        return lambda variables={}, parse_result=None: {}
 
     # Create a copy of the templates_dict to avoid modification of the original
     templates_dict = templates_dict.copy()
