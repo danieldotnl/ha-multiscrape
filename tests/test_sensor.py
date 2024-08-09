85e7818dcd0311dc6b7a6025b9c003b5e7a7b5a1
danieldotnl <danieldotnl@outlook.com>
2024-08-09 12:46:30 +0000

    Setup test infra

---
+++ tests/test_sensor.py
@@ -0,0 +1,9 @@
+"""Tests for multiscrape sensor."""
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant
+
+
+async def test_scrape_sensor(hass: HomeAssistant) -> None:
+    """Test the scrape sensor."""
+    return True
